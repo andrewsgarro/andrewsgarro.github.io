baa46bb40a1d5ba2159592a4603d829464a37728
Andrew Sgarro <andrew.sgarro@wustl.edu>
2016-05-19 00:03:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ var main = function() {
     nextDot.addClass('active-dot');
 
   });
-
+  
   $('.prev-arrow').click(function() {
     var currentSlide = $('.active-slide');
     var prevSlide = currentSlide.prev();
@@ -80,7 +80,57 @@ var main = function() {
     prevDot.addClass('active-dot');
 
   });
+  
+  $(document).keypress(function(e) {
+    if (e.keyCode == 37) {
+      var currentSlide = $('.active-slide');
+      var prevSlide = currentSlide.prev();
 
+      var currentDot = $('.active-dot');
+      var prevDot = currentDot.prev();
+
+      if(prevSlide.length === 0){
+       prevSlide = $('.slide').last();
+       prevDot = $('.dot').last();
+      }
+
+     currentSlide.fadeOut(0).removeClass('active-slide');
+     prevSlide.fadeIn(300).addClass('active-slide');
+
+      $('.active-slide').css("display", "flex");
+      $(".book-image").show();
+      $(".book-description").hide();
+      $(".author-image").show();
+      $(".author-description").hide();
+
+    currentDot.removeClass('active-dot');
+    prevDot.addClass('active-dot');
+      
+    } else if (e.keyCode == 39) {
+       var currentSlide = $('.active-slide');
+    var nextSlide = currentSlide.next();
+
+    var currentDot = $('.active-dot');
+    var nextDot = currentDot.next();
+
+    if(nextSlide.length === 0){
+      nextSlide = $('.slide').first();
+      nextDot = $('.dot').first();
+    }
+
+    currentSlide.fadeOut(0).removeClass('active-slide');
+    nextSlide.fadeIn(300).addClass('active-slide');
+
+    $('.active-slide').css("display", "flex");
+    $(".book-image").show();
+    $(".book-description").hide();
+    $(".author-image").show();
+    $(".author-description").hide();
+
+      currentDot.removeClass('active-dot');
+      nextDot.addClass('active-dot');
+    }
+  });
 }
 
 $(document).ready(main);
